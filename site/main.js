552bd6b6983478715a300e9d804cd48b39e11294
Rodrigo Cerna <rod9231@gmail.com>
2020-11-29 18:20:19 -0500

    adding behavior

---
+++ site/main.js
@@ -0,0 +1,6 @@
+const first_p = document.getElementById("first");
+const second_p = document.getElementById("second");
+
+function change_color(element) {
+    element.style.backgroundColor = "white";
+}
